7c0eba98a63f2e2ad68ebf71663945f41b9c6968
ShjnAki <cryptologya@hotmail.com>
2026-06-05 21:45:07 +0200

    fix(mobile): API via HTTPS DuckDNS

--- mobile/src/config.js
+++ mobile/src/config.js
@@ -1,3 +1,3 @@
 // Remplacer IP_VPS par l'adresse IP réelle du serveur, ou le domaine quand disponible
 // Exemple : "http://192.168.1.100:8000" ou "https://api.tondomaine.com"
-export const API_URL = "http://207.180.249.222:8001";
+export const API_URL = "https://code-shinnamon.duckdns.org";
